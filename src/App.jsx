e68012a3229b6ccdc26e73f82d83e4d2c6c924e7
burak <mbyilmazer38@gmail.com>
2022-07-01 23:25:41 +0300

    123

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,28 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
-import Card from "./components/Card";
-import "./App.scss";
-
+import axios from 'axios';
+import React, {useState, useEffect} from 'react'
+import Card from './components/Card';
+import './App.scss'
 const url = "https://randomuser.me/api/";
-
 const App = () => {
-  const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState([]);
-
-  const users = async () => {
+   const [user, setUser] = useState("");
+   const users = async () =>{
     try {
-      const response = await axios.get(url);
-
-      const { data } = response;
-      setUser(data.results[0]);
-      setLoading(false);
+        const {data} = await axios.get(url);
+        setUser(data.results[0]);
     } catch (error) {
-      alert(error);
+        alert(error);
     }
-  };
-
-  useEffect(() => {
+   }
+   useEffect(() =>{
     users();
-  }, []);
-  if (loading) {
-    return <h2>Loading...</h2>;
-  }
-
+   }, [])
   return (
-    <div>
-      <div className="App">
-        <Card user={user} />
-      </div>
-      <button className="button" onClick={() => users()}> random user</button>
-    </div>
-  );
-};
-
-export default App;
+    <>
+        <div className='App'>
+            {user && <Card user = {user}/>}
+        </div>
+        <button className='button' onClick={users}>Random User</button>
+    </>
+  )
+}
+export default App
